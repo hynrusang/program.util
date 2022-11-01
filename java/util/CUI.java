a7b2fe7d2ae17df3e36206c359b419380f6637bc
hynrusang <mashmalo1871@gmail.com>
2022-11-01 13:15:57 +0900

    up

---
+++ java/util/CUI.java
@@ -0,0 +1,9 @@
+package util;
+import util.file.*;
+
+final public class CUI {
+	public static void start() {
+		
+	}
+	private CUI() { }
+}
--- java/util/GUI.java
+++ java/util/GUI.java
@@ -1,9 +1,28 @@
 package util;
 import util.file.*;
+import java.awt.*;
+import javax.swing.*;
 
-final public class GUI {
+class _MenuBar extends JMenuBar {
+	public static JMenuBar get() {
+		JMenuBar mb = new JMenuBar(); 
+		JMenu screenMenu = new JMenu("핸들러");
+		screenMenu.add(new JMenuItem("U.Database"));
+		screenMenu.addSeparator();
+		screenMenu.add(new JMenuItem("U.file.Filepath"));
+		screenMenu.add(new JMenuItem("U.file.Json"));
+		mb.add(screenMenu);
+		return mb;
+	}
+	private _MenuBar() { }
+}
+final public class GUI extends JFrame {
 	public static void start() {
-		
+		GUI main = new GUI();
+		main.setTitle("util 패키지 매니저");
+		main.setJMenuBar(_MenuBar.get());;
+		main.setSize(1040, 720);
+		main.setVisible(true);
 	}
 	private GUI() { }
 }
